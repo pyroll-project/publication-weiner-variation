d9f6618e94e2ccf101943cdf21a059dafb004574
Max Weiner <max.weiner@imf.tu-freiberg.de>
2023-05-26 16:14:08 +0200

    enh data extraction

--- weiner_variation/data/task_extract_pass_data.py
+++ weiner_variation/data/task_extract_pass_data.py
@@ -24,52 +24,81 @@ for material in MATERIALS:
             raw_data.index = pd.to_datetime(raw_data.index)
             raw_data = raw_data.resample("10ms").mean()
 
-            def _extract_force_and_torque(p: pd.DataFrame, key: str):
-                p["roll_force"] = p.apply(
-                    lambda row: raw_data[f"roll_force_{key}"][
-                        (raw_data.index > row.start) & (raw_data.index < row.end)].median(),
-                    axis=1
-                )
-                p["roll_torque"] = p.apply(
-                    lambda row: np.abs(raw_data[f"roll_torque_{key}"][
-                                           (raw_data.index > row.start) & (raw_data.index < row.end)]).median() / 2,
-                    axis=1
-                )
+            def _extract_force_or_torque(key: str, delta=pd.Timedelta("500ms")):
+                def _get(row):
+                    idx_plateau = (raw_data.index > row.start) & (raw_data.index < row.end)
+                    plateau = raw_data[key][idx_plateau].median()
+                    idx_base = (raw_data.index > row.start - delta) & (raw_data.index < row.start)
+                    base = raw_data[key][idx_base].median()
+                    return np.abs(plateau - base)
 
-            def _extract_duo_temperatures(row):
-                if row.name % 2 == 0:
-                    t = raw_data["temp_3"]
-                else:
-                    t = raw_data["temp_2"]
+                return _get
 
-                return t[
-                    (raw_data.index > row.start - pd.Timedelta(500, "ms"))
-                    & (raw_data.index < row.start - pd.Timedelta(200, "ms"))
-                    & (t > MIN_TEMP)
-                    ].median()
+            def _extract_duo_temperature_in(delta=pd.Timedelta("500ms")):
+                def _get(row):
+                    if row.name % 2 == 0:
+                        t_in = raw_data["temp_3"]
+                    else:
+                        t_in = raw_data["temp_2"]
+
+                    idx_in = (raw_data.index > row.start - delta) & (raw_data.index < row.start)
+                    return t_in[idx_in & (t_in > MIN_TEMP)].median()
+
+                return _get
+
+            def _extract_duo_temperature_out(delta=pd.Timedelta("500ms")):
+                def _get(row):
+                    if row.name % 2 == 0:
+                        t_out = raw_data["temp_2"]
+                    else:
+                        t_out = raw_data["temp_3"]
+
+                    idx_out = (raw_data.index > row.end) & (raw_data.index < row.end + delta)
+                    return t_out[idx_out & (t_out > MIN_TEMP)].median()
+
+                return _get
+
+            def _extract_f_temperature_in(i, delta=pd.Timedelta("500ms")):
+                def _get(row):
+                    t_in = raw_data[f"temp_{4 + i}"]
+                    idx_in = (raw_data.index > row.start - delta) & (raw_data.index < row.start)
+                    return t_in[idx_in & (t_in > MIN_TEMP)].median()
+
+                return _get
+
+            def _extract_f_temperature_out(i, delta=pd.Timedelta("500ms")):
+                def _get(row):
+                    t_out = raw_data[f"temp_{5 + i}"]
+                    idx_out = (raw_data.index > row.end) & (raw_data.index < row.end + delta)
+                    return t_out[idx_out & (t_out > MIN_TEMP)].median()
+
+                return _get
 
             duo_passes = find_passes(id, raw_data["roll_torque_duo"], PROMINENCE_DUO, 10)
-            _extract_force_and_torque(duo_passes, "duo")
+            duo_passes["roll_force"] = duo_passes.apply(_extract_force_or_torque("roll_force_duo"), axis=1)
+            duo_passes["roll_torque"] = duo_passes.apply(_extract_force_or_torque("roll_torque_duo"), axis=1)
 
-            duo_passes["out_temperature"] = duo_passes.apply(_extract_duo_temperatures, axis=1)
+            duo_passes["in_temperature"] = duo_passes.apply(_extract_duo_temperature_in(), axis=1)
+            duo_passes["out_temperature"] = duo_passes.apply(_extract_duo_temperature_out(), axis=1)
 
             f_passes = list(range(4))
             for i in range(4):
                 f_passes[i] = find_passes(id, raw_data[f"roll_torque_f{i + 1}"], PROMINENCE_F, 1)
                 if not f_passes[i].empty:
-                    _extract_force_and_torque(f_passes[i], f"f{i + 1}")
+                    f_passes[i]["roll_force"] = f_passes[i].apply(
+                        _extract_force_or_torque(f"roll_force_f{i + 1}"),
+                        axis=1
+                    )
+                    f_passes[i]["roll_torque"] = f_passes[i].apply(
+                        _extract_force_or_torque(f"roll_torque_f{i + 1}"),
+                        axis=1
+                    )
                     f_passes[i]["in_temperature"] = f_passes[i].apply(
-                        lambda row: raw_data[f"temp_{4 + i}"][
-                            (raw_data.index > row.start) & (raw_data.index < row.end) & (
-                                        raw_data[f"temp_{4 + i}"] > MIN_TEMP)
-                            ].median(),
+                        _extract_f_temperature_in(i),
                         axis=1
                     )
                     f_passes[i]["out_temperature"] = f_passes[i].apply(
-                        lambda row: raw_data[f"temp_{5 + i}"][
-                            (raw_data.index > row.start) & (raw_data.index < row.end) & (
-                                        raw_data[f"temp_{5 + i}"] > MIN_TEMP)
-                            ].median(),
+                        _extract_f_temperature_out(i),
                         axis=1
                     )
 
