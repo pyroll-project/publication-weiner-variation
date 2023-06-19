381201475ae172fafe1f911d6ae0152781f04b40
Max Weiner <max.weiner@imf.tu-freiberg.de>
2023-06-19 14:13:46 +0200

    use papermill for simulations

--- weiner_variation/sim/task_sim_durations.py
+++ weiner_variation/sim/task_sim_durations.py
@@ -8,20 +8,23 @@ from weiner_variation.config import DATA_DIR
 
 @pytask.mark.task()
 @pytask.mark.depends_on([
-    "sim_durations.py",
+    "sim_durations.ipynb",
     DATA_DIR / "input_dist.csv",
     DATA_DIR / "duo_pauses_dist.csv",
     "config.py",
     "process.py",
 ])
 @pytask.mark.produces(DATA_DIR / "sim_durations_results.csv")
-def task_sim_durations():
+def task_sim_durations(depends_on: dict[..., Path]):
     result = subprocess.run(
         [
             "hatch",
             "run",
-            "sim:python",
-            "-m", "weiner_variation.sim.sim_durations",
+            "sim:papermill",
+            "-l", "python",
+            "--stdout-file", str(depends_on[0].with_suffix(".log")),
+            str(depends_on[0]),
+            str(depends_on[0].with_suffix(".out.ipynb"))
         ]
     )
 
--- weiner_variation/sim/task_sim_input.py
+++ weiner_variation/sim/task_sim_input.py
@@ -1,3 +1,4 @@
+import json
 import subprocess
 
 import pytask
@@ -7,15 +8,18 @@ from weiner_variation.config import DATA_DIR
 
 
 @pytask.mark.task()
-@pytask.mark.depends_on(["sim_input.py", DATA_DIR / "input_dist.csv", "config.py", "process.py"])
+@pytask.mark.depends_on(["sim_input.ipynb", DATA_DIR / "input_dist.csv", "config.py", "process.py"])
 @pytask.mark.produces(DATA_DIR / "sim_input_results.csv")
-def task_sim_input(depends_on: Path, produces: Path):
+def task_sim_input(depends_on: dict[..., Path]):
     result = subprocess.run(
         [
             "hatch",
             "run",
-            "sim:python",
-            "-m", "weiner_variation.sim.sim_input",
+            "sim:papermill",
+            "-l", "python",
+            "--stdout-file", str(depends_on[0].with_suffix(".log")),
+            str(depends_on[0]),
+            str(depends_on[0].with_suffix(".out.ipynb"))
         ]
     )
 
