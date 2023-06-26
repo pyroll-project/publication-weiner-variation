7576a3d2d25a135beb21b1535f12d6c80b2f9024
Max Weiner <max.weiner@imf.tu-freiberg.de>
2023-06-26 09:42:44 +0200

    make sim nominal a notebook

--- weiner_variation/sim/task_sim_nominal.py
+++ weiner_variation/sim/task_sim_nominal.py
@@ -3,18 +3,24 @@ import subprocess
 import pytask
 from pathlib import Path
 
+from weiner_variation.config import DATA_DIR
+
 
 @pytask.mark.task()
-@pytask.mark.depends_on(["sim_nominal.py", "config.py", "process.py"])
-@pytask.mark.produces("report.html")
-def task_sim_nominal(depends_on: Path, produces: Path):
+@pytask.mark.depends_on(["sim_nominal.ipynb", "config.py", "process.py"])
+@pytask.mark.produces(DATA_DIR / "sim_nominal_results.csv")
+def task_sim_nominal(depends_on: dict[..., Path]):
     result = subprocess.run(
         [
             "hatch",
             "run",
-            "sim:python",
-            "-m", "weiner_variation.sim.sim_nominal",
+            "sim:papermill",
+            "-l", "python",
+            "--stdout-file", str(depends_on[0].with_suffix(".log")),
+            str(depends_on[0]),
+            str(depends_on[0].with_suffix(".out.ipynb"))
         ]
     )
 
     result.check_returncode()
+
